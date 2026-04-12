a3c85ec0383c05bfa9d7b3f42e881f6edf16eadf
Lidia Luceño <lidialucenopalou@hotmail.com>
2026-04-12 15:14:09 +0200

    Update container.js

--- src/container.js
+++ src/container.js
@@ -3,11 +3,16 @@ import { ExceptionService } from "./services/exception.service.js";
 import { SessionService } from "./services/session.service.js";
 import { LoginService } from "./services/login.service.js";
 import { UserService } from "./services/user.service.js";
+import { DietService } from "./services/diet.service.js";
+import { MealLogService } from "./services/meallog.service.js";
+import { WeightLogService } from "./services/weightlog.service.js";
 import { ExceptionView } from "./views/exception.view.js";
 import { LoginView } from "./views/login.view.js";
 import { UserView } from "./views/user.view.js";
+import { RegisterView } from "./views/register.view.js";
 import { LoginController } from "./controllers/login.controller.js";
 import { UserController } from "./controllers/user.controller.js";
+import { RegisterController } from "./controllers/register.controller.js";
 
 export class Container {
   #props = {};
@@ -16,6 +21,8 @@ export class Container {
   constructor(config) {
     this.#config = config;
   }
+
+  // ─── Servicios base ────────────────────────────────────────────
 
   get httpService() {
     if (this.#props.httpService) return this.#props.httpService;
@@ -41,6 +48,8 @@ export class Container {
     return this.#props.sessionService;
   }
 
+  // ─── Servicios de dominio ──────────────────────────────────────
+
   get loginService() {
     if (this.#props.loginService) return this.#props.loginService;
     this.#props.loginService = new LoginService(this.httpService, this.sessionService);
@@ -53,6 +62,26 @@ export class Container {
     return this.#props.userService;
   }
 
+  get dietService() {
+    if (this.#props.dietService) return this.#props.dietService;
+    this.#props.dietService = new DietService(this.httpService, this.sessionService);
+    return this.#props.dietService;
+  }
+
+  get mealLogService() {
+    if (this.#props.mealLogService) return this.#props.mealLogService;
+    this.#props.mealLogService = new MealLogService(this.httpService, this.sessionService);
+    return this.#props.mealLogService;
+  }
+
+  get weightLogService() {
+    if (this.#props.weightLogService) return this.#props.weightLogService;
+    this.#props.weightLogService = new WeightLogService(this.httpService, this.sessionService);
+    return this.#props.weightLogService;
+  }
+
+  // ─── Vistas ────────────────────────────────────────────────────
+
   get loginView() {
     if (this.#props.loginView) return this.#props.loginView;
     this.#props.loginView = new LoginView(this.exceptionView);
@@ -64,6 +93,14 @@ export class Container {
     this.#props.userView = new UserView();
     return this.#props.userView;
   }
+
+  get registerView() {
+    if (this.#props.registerView) return this.#props.registerView;
+    this.#props.registerView = new RegisterView(this.exceptionView);
+    return this.#props.registerView;
+  }
+
+  // ─── Controladores ────────────────────────────────────────────
 
   get loginController() {
     if (this.#props.loginController) return this.#props.loginController;
@@ -83,4 +120,16 @@ export class Container {
     );
     return this.#props.userController;
   }
+
+  get registerController() {
+    if (this.#props.registerController) return this.#props.registerController;
+    this.#props.registerController = new RegisterController(
+      this.userService,
+      this.weightLogService,
+      this.exceptionService,
+      this.registerView
+    );
+    return this.#props.registerController;
+  }
 }
+
