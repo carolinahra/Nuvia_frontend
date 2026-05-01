d6a1dc08301255366f01bf75023c93ea87afb280
Carolina Herrera <carolinahradiaz@gmail.com>
2026-05-01 20:09:34 +0200

    added new templates routes to vite config

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,11 @@ export default defineConfig({
         registro: resolve(__dirname, 'templates/registro.html'),
         contacto: resolve(__dirname, 'templates/contacto.html'),
         entrenamiento: resolve(__dirname, 'templates/entrenamiento.html'),
+        alimentacion: resolve(__dirname, 'templates/alimentacion.html'),
+        perfil: resolve(__dirname, 'templates/perfil.html'),
+        editarPerfil: resolve(__dirname, 'templates/editar-perfil.html'),
+        calorias: resolve(__dirname, 'templates/calorias.html'),
+        estadisticas: resolve(__dirname, 'templates/estadisticas.html'),
       }
     }
   }
